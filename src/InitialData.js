4e3a6538dae8985b3639bd022fd9c103c48924c7
MD AHMED RAZA <ahmedraza8292@gmail.com>
2023-04-08 09:14:12 +0530

    Add files via upload

---
+++ src/InitialData.js
@@ -0,0 +1,44 @@
+var studentArray = [{
+    id: 1,
+    name: 'Aman',
+    currentClass: 5,
+    division: 'A'
+},
+{
+    id: 2,
+    name: 'Rahul',
+    currentClass: 12,
+    division: 'C'
+},
+{
+    id: 3,
+    name: 'Sujit',
+    currentClass: 8,
+    division: 'C'
+},
+{
+    id: 4,
+    name: 'Himayou',
+    currentClass: 7,
+    division: 'A'
+},
+{
+    id: 5,
+    name: 'Farhan',
+    currentClass: 9,
+    division: 'D'
+},
+{
+    id: 6,
+    name: 'Khwaza',
+    currentClass: 10,
+    division: 'C'
+},
+{
+    id: 7,
+    name: 'Komal',
+    currentClass: 10,
+    division: 'A'
+}]
+
+module.exports = studentArray;
